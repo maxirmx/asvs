9aaff8cd330484cdae24367815629883a1670d2a
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2022-05-24 18:16:40 +0300

    Testing 1

--- tests/tests.sh
+++ tests/tests.sh
@@ -11,10 +11,19 @@ test_start_app() {
 }
 
 test_sp_account() {
-   curl --header "Content-Type: application/json" --data  "{ \"sp_customer_uuid\": \"4f4867be-0001-4670-9148-3a9f5c70015e\", \"dnc_lrn_replacement\":\"LRN1\", \"started_on\":\"1917-08-01 12:15:01\", \"expired_on\":\"2199-12-31 15:12:59\", \"active\": true, \"cps_limit\": 12 }" http://localhost:8080/api/customer_create
-   curl --header "Content-Type: application/json" --data  "{ \"sp_customer_uuid\": \"4f4867be-0002-4670-9148-3a9f5c70015e\", \"dnc_lrn_replacement\":\"LRN2\", \"started_on\":\"1917-08-01 12:15:01\", \"expired_on\":\"2199-12-31 15:12:59\", \"active\": false, \"cps_limit\": 12 }" http://localhost:8080/api/customer_create
-   curl --header "Content-Type: application/json" --data  "{ \"sp_customer_uuid\": \"4f4867be-0003-4670-9148-3a9f5c70015e\",  \"started_on\":\"1917-08-01 12:15:01\", \"expired_on\":\"2199-12-31 15:12:59\", \"active\": true, \"cps_limit\": 1111, \"as_enabled\": false, \"vs_enabled\": false}" http://localhost:8080/api/customer_create
-   curl --header "Content-Type: application/json" --data  "{ \"sp_customer_uuid\": \"4f4867be-0100-4670-9148-3a9f5c70015e\",  \"started_on\":\"1917-08-01 12:15:01\", \"expired_on\":\"2199-12-31 15:12:59\", \"active\": true, \"cps_limit\": 12 }" http://localhost:8080/api/customer_create
+   ! IFS= read -r -d '' data1 << EOM
+      { 
+         "sp_customer_uuid": "4f4867be-0001-4670-9148-3a9f5c70015e", 
+         "dnc_lrn_replacement":"LRN1", 
+         "started_on":"1917-08-01 12:15:01", 
+         "expired_on":"2199-12-31 15:12:59", "active": true, 
+         "cps_limit": 12 
+      }   
+EOM
+   curl -s --header "Content-Type: application/json" --data "$data1"  http://localhost:8080/api/customer_create
+   curl -s --header "Content-Type: application/json" --data  "{ \"sp_customer_uuid\": \"4f4867be-0002-4670-9148-3a9f5c70015e\", \"dnc_lrn_replacement\":\"LRN2\", \"started_on\":\"1917-08-01 12:15:01\", \"expired_on\":\"2199-12-31 15:12:59\", \"active\": false, \"cps_limit\": 12 }" http://localhost:8080/api/customer_create
+   curl -s --header "Content-Type: application/json" --data  "{ \"sp_customer_uuid\": \"4f4867be-0003-4670-9148-3a9f5c70015e\",  \"started_on\":\"1917-08-01 12:15:01\", \"expired_on\":\"2199-12-31 15:12:59\", \"active\": true, \"cps_limit\": 1111, \"as_enabled\": false, \"vs_enabled\": false}" http://localhost:8080/api/customer_create
+   curl -s --header "Content-Type: application/json" --data  "{ \"sp_customer_uuid\": \"4f4867be-0100-4670-9148-3a9f5c70015e\",  \"started_on\":\"1917-08-01 12:15:01\", \"expired_on\":\"2199-12-31 15:12:59\", \"active\": true, \"cps_limit\": 12 }" http://localhost:8080/api/customer_create
 }
 
 test_stop_app() {
